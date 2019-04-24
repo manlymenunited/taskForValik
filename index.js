65e0f69613b8ce6b6ee17d22f016d16255d6105e
Лавращук Павел Иванович <lavraschuk@Milavitsa.local>
2019-04-24 21:12:47 +0300

    v1.0

---
+++ index.js
@@ -0,0 +1,53 @@
+var osmosis = require('osmosis');
+var Excel = require('exceljs');
+
+var resultArr = [];
+var workbook = new Excel.Workbook();
+var sheet = workbook.addWorksheet('Sheet1');
+sheet.columns = [
+    { header: 'id', key: 'id' },
+    { header: 'type', key: 'type' },
+    { header: 'name', key: 'name' },
+    { header: 'lNum', key: 'lNum' },
+    { header: 'licenceNum', key: 'licenceNum' },
+    { header: 'licenceDate', key: 'licenceDate' },
+    { header: 'customCode', key: 'customCode' },
+    { header: 'address', key: 'address' },
+]
+
+var counts = 0;
+url = "https://www.alta.ru/svh/";
+
+osmosis
+    .get(url)
+    .paginate("//div[@class='pagination']/table//td[a[@href='/svh/page_84/']]/a[1]", 83)
+    .find("//div[@class='boxSubstrate boxSubstrate-offset-0 p-10']")
+    .set({
+        type: "text()",
+        name: "div[@class='h3']",
+        license: "div[@class='pSvh_fieldColumn pSvh_fieldColumn-list pSvh_fieldColumn-right']",
+        address: "div[@class='pSvh_fieldColumn pSvh_fieldColumn-list pSvh_fieldColumn-left lightgray']",
+        customCode: "div[@class='pSvh_fieldColumn pSvh_fieldColumn-list pSvh_fieldColumn-left']",
+    })
+    .log(console.log)
+    .data((data) => { resultArr.push(data); })
+    .done(() => {
+        for (var i = 0; i < resultArr.length; i++) {
+            var arrLicense = new String(resultArr[i].license).split("\n");
+            for (var k = 0; k < arrLicense.length; k++) {
+                var myLicense = new String(arrLicense[k]).trim();
+                sheet.addRow({
+                    id: i,
+                    type: resultArr[i].type.replace(/^\s+/g, ""),
+                    name: resultArr[i].name.replace(/^\s+/g, ""),
+                    lNum: k + 1,
+                    licenceNum: myLicense.substr(0, myLicense.indexOf("действует")),
+                    licenceDate: myLicense.substr(myLicense.indexOf("действует") + 12),
+                    customCode: resultArr[i].customCode.replace(/^\s+/g, ""),
+                    address: resultArr[i].address.replace(/^\s+/g, "")
+                })
+            }
+        }
+        workbook.xlsx.writeFile("valikTask.xlsx").then(function () { });
+    })
+    ;
